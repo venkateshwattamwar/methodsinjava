0c906a5b8420ea5f1343cb6960dc7012401d9ced
ADMIN$ <ADMIN$@DESKTOP-UVVSPQO>
2022-05-21 13:20:43 +0530

    methods in java

--- src/practice/B.java
+++ src/practice/B.java
@@ -1,6 +1,6 @@
 package practice;
 
-public  class B extends A {//whenever we declare a class as final then we are not be able to create its child class
+//public  class B extends A {//whenever we declare a class as final then we are not be able to create its child class
 
 
-}
+//}
